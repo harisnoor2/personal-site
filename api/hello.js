e2e7a9f2b9fea05fcb77646fe9c19f3c82a01813
s <156768148+harisnoor2@users.noreply.github.com>
2025-09-23 23:22:21 -0600

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,3 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "Hello, world!" });
-}
+module.exports = (req, res) => {
+  res.status(200).json({ message: "Hello from Vercel!" });
+};
